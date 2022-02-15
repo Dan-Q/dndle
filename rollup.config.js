3544ac92ab5872b4732c1d90323976d16c559f8d
Dan Q <github@danq.me>
2022-02-15 22:15:59 +0100

    Remove unneeded rollup plugin.

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,6 @@
 // rollup.config.js
-import html from "rollup-plugin-html";
-
 export default {
   input: "src/dndle.js",
-  plugins: [
-    html({
-      include: "templates/*.html",
-    }),
-  ],
   output: {
     file: "public/dndle.js",
     compact: true,
